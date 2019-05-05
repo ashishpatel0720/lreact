13dc393f25b1eb9942bd30a00f9d7f3669e193a6
Ashish Patel <ashishpatel0720@gmail.com>
2019-05-05 13:11:03 +0530

    Starting revision

--- src/App.js
+++ src/App.js
@@ -5,42 +5,12 @@ import Person from "./components/Person";
 
 class App extends Component {
 
-    state={
-        user1:{
-            name:"Ashish",
-            age:10
-        },
-        user2:{
-            name:"Patel",
-            age:20
-        },
 
+    render() {
+        return (
+            <h1>Hello</h1>
+        );
     }
-
-
-    interchangeHandler=(v)=>{
-    console.log(v);
-        let s={...this.state};
-
-        let temp=s.user1;
-        s.user1=s.user2;
-        s.user2=temp;
-        this.setState(s);
-    }
-  render() {
-    return (
-      <div>
-       <Person name={this.state.user1.name}  click={(v)=>this.interchangeHandler(v)} age={this.state.user1.age}>
-          This i some information about that person as well
-       </Person>
-          <Person name={this.state.user2.name}  click={(v)=>this.interchangeHandler(v)} age={this.state.user2.age}>
-              Patel is greate!
-       </Person>
-          <button onClick={this.interchangeHandler}>InterChange</button>
-
-      </div>
-    );
-  }
 }
 
 export default App;
